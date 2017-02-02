98d98bcf5e97de31f922d8bf08b20d7da96dc7c4
Arjen Wassink <awassink@quintor.nl>
2017-02-02 13:16:53 +0100

    Setup to use Java 7 in order to prevent Scala issues with Java 8 causing activator-ui to fail.

---
+++ env.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export PATH=/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home/bin/:$PATH
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.7.0_67.jdk/Contents/Home/
